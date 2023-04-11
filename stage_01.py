9041586d9512ea43e7c52835e854f9c579479ce7
Saeed Alig <gj5404@myamu.ac.in>
2023-04-11 19:29:56 +0530

    Files Added

---
+++ stage_01.py
@@ -0,0 +1,4 @@
+text = "demo for dvc "
+
+with open('artifaction.txt', 'w') as f:
+    f.write(text)
---
+++ stage_02.py
@@ -0,0 +1,5 @@
+with open("artifaction.txt", "r") as f:
+    text = f.read()
+
+print(text)
+print("end of stage-02")
